9892decf91fe68128e6bef8478103680b2ee8af8
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-11-07 15:23:58 -0500

    added multiFileParser

---
+++ multiFileCensusParser.py
@@ -0,0 +1,39 @@
+import glob
+path = "CensusData/ny_census/*.csv"
+
+map = {}
+
+for fpath in glob.glob(path):
+    fileName = fpath.replace("CensusData/ny_census/", '')
+
+    input = open(fpath, 'r')
+
+    for line in input:
+        line = line.replace('\"','')
+        line = line.split(',')
+
+        if line[0].lower() == 'people' and line[1].lower()== 'race':
+            if line[2].lower() != 'one race' and line[2].lower() != "two or more races":
+                if line[2] in map:
+                    temp = map[line[2]]
+                    temp.append(line[3])
+                    map[line[2]] = temp
+                else:
+                    temp = []
+                    temp.append(line[3])
+                    map[line[2]] = temp
+
+
+    input.close()
+
+output = open("parsedFiles/nyCensus.csv", 'w')
+
+for key in map:
+    line = str(key) + ','
+    for item in map[key]:
+        line += str(item) + ','
+    line = line[:-1]
+    line += '\n'
+    output.write(line)
+
+output.close()
