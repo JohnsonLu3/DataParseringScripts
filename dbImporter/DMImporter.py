124918ebf1ad60db65cf9e9174d33579995451a6
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-11-10 22:42:01 -0500

    reformatted structure of code and fixed bugs

--- dbImporter/DMImporter.py
+++ dbImporter/DMImporter.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
+from sqlalchemy.sql import select
 from sqlalchemy import text
 from sqlalchemy.ext.automap import automap_base
 
@@ -15,26 +15,38 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 Boundaries = Base.classes.Boundaries
+States = Base.classes.States
+Districts = Base.classes.Districts
+Population = Base.classes.Population
 
-StateBoundaries = []
-states = []
-districts = []
-DistrictBoundaries = []
-boundaries = []
-population = []
 
+stateFPs = {}
 
 def main():
 
     # printTables()
-    importData()
+    buildStateFPMap()
+    #importStateData()
+    importStateBoundaryData()
 
-def importData():
+
+def buildStateFPMap():
+    stateData = open("../parsedFiles/StateGeo.csv", 'r')
+
+    for line in stateData:
+        line = line.split(';')
+        if line[1] not in stateFPs.keys():
+            stateFPs[line[1]] = line[0]
+
+
+    stateData.close()
+
+
+def importStateData():
     # populate voting data
     #
     voteData  = open("../parsedFiles/votingData.csv", 'r')
-    stateData = open("../parsedFiles/StateGeo.csv", 'r')
-
+    importedStatesYear = []
 
     for line in voteData:
         data = []
@@ -47,87 +59,89 @@ def importData():
 
         if len(data) == 5:
             # import data into database
-            sId     = -1
-            polygons = []
             sName   = data[0]
             year    = data[1]
-            dId     = data[2]
-            rVote   = data[3]
-            dVote   = data[4]
 
-            for line in stateData:
-                line = line.split(';')
-                if line[1] == sName:
-                    sId = int(line[0])
-                    # get polygons
-                    for i in range(2,len(line)):
-                        polygon = line[i]
-                        polygon.replace('\n', '')
-                        polygon = "POLYGON(" + polygon + ")"
+            stateYear = sName+str(year)
+            if stateYear not in importedStatesYear:
+                # get StateFp
+                sId = stateFPs[sName]
 
-                        polygons.append(polygon)
-                    break
-
-            # import State Data
-            ins = metadata.tables['States'].insert().values(StateName = sName , Year = int(year), ClickCount = 0)
-            result = conn.execute(ins)
-            statePKId = result.inserted_primary_key
-
-            # import State Boundaries
-            boundaryPKId = []
-
-            for polygon in polygons:
-                polygon = "PolygonFromText(\'" + polygon + "\')"
-
-                # sql = text('insert into Boundaries(Shape) VALUES(' + polygon +')')
-                # conn.execute(sql)
-
-                ins = metadata.tables['Boundaries'].insert().values(Shape=text(polygon))
-                result = conn.execute(ins)
-                boundPKId = result.inserted_primary_key
-
-                # s  = select([Column('Id')]).where(Boundaries.Shape == polygon)
-                # pk = conn.execute(s)
-                # get pkId from last inserted for StateBoundary FK
-                boundaryPKId.append(boundPKId)
-
-            # import StateBoundaries
-            for pkId in boundaryPKId:
-                ins = metadata.tables['StateBoundaries'].insert().values(BoundaryId = pkId, StateId = statePKId)
-                result = conn.execute(ins)
-
-            # import district Data
-            importDistricts(statePKId, sId, dId)
-            importPopulationData(year, sName, sId, dId)
-
-
-            # import votingData
-            importVotingData(dId, "Democrat", rVote)
-            importVotingData(dId, "Republican", rVote)
+                # import State Data
+                ins = metadata.tables['States'].insert().values(StateId=sId, StateName=sName, Year=int(year), ClickCount=0)
+                conn.execute(ins)
+                importedStatesYear.append(sName+year)
 
     voteData.close()
+
+def importStateBoundaryData():
+    # import State Boundaries
+    stateData = open("../parsedFiles/StateGeo.csv", 'r')
+    boundaryPKId = []
+
+    for line in stateData:
+        line = line.split(';')
+        sId = int(line[0])
+        polygons = line[2].split(';')
+
+        for polygon in polygons:
+            polygon = "PolygonFromText(\'POLYGON(" + polygon + ")\')"
+
+            # sql = text('insert into Boundaries(Shape) VALUES(' + polygon +')')
+            # conn.execute(sql)
+
+            ins = metadata.tables['Boundaries'].insert().values(Shape=text(polygon))
+            result = conn.execute(ins)
+            boundPKId = result.inserted_primary_key
+
+            # get pkId from last inserted for StateBoundary FK
+            boundaryPKId.append(boundPKId)
+
+        # import StateBoundaries
+        for pkId in boundaryPKId:
+            ins = metadata.tables['StateBoundaries'].insert().values(BoundaryId=pkId[0], StateId=sId)
+            result = conn.execute(ins)
+
+
     stateData.close()
 
-def importDistricts(statePKId, sId, dId):
+def importDistricts(statePKId, year, sId):
 
     # Districts
     # `DistrictId`
     # `Area`
     # `clickCount`
     # `StateId` FK
-    district2013Data = open('../parsedFiles/DistrictGeo_2013.csv','r')
-    for line in district2013Data:
+    area = None
+    districtPKs = []
+    districtData = None
+
+    if year == 2013:
+        districtData = open('../parsedFiles/DistrictGeo_2013.csv', 'r')
+    elif year == 2014:
+        districtData = open('../parsedFiles/DistrictGeo_2014.csv', 'r')
+
+    elif year == 2016:
+        districtData = open('../parsedFiles/DistrictGeo_2016.csv', 'r')
+    else:
         pass
 
-    district2014Data = open('../parsedFiles/DistrictGeo_2014.csv', 'r')
-    for line in district2014Data:
+    if districtData != None:
+        for line in districtData:
+            line = line.split(';')
+            if line[0] == sId and line[1] == dId:
+                # Insert District Data
+                ins = metadata.tables['Districts'].insert().values(DistrictId = dId, Area= area, clickCount = 0, StateId=statePKId)
+                result = conn.execute(ins)
+                boundPKId = result.inserted_primary_key
+
+                # get pkId from last inserted for StateBoundary FK
+                districtPKs.append(boundPKId)
+    else:
+        # No district data
         pass
 
-    district2016Data = open('../parsedFiles/DistrictGeo_2016.csv', 'r')
-    for line in district2016Data:
-        pass
-
-
+    return districtPKs
 
 
 def importPopulationData(year, sName, sFp, district):
